ead0633e210492916bb0af131807a7dd337a56d0
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-03-04 21:13:38 +0000

    add ability to view / add trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,8 +4,29 @@ class TripsController < ApplicationController
   end
 
   def show
+    @trip = Trip.find(params[:id])
+    authorize @trip
   end
 
   def new
+    @trip = Trip.new
+    authorize @trip
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    @trip.lead_user = current_user
+    authorize @trip
+    if @trip.save
+      redirect_to trip_path(@trip)
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:name, :description, :start_date, :end_date, :city_id)
   end
 end
--- app/policies/trip_policy.rb
+++ app/policies/trip_policy.rb
@@ -4,4 +4,12 @@ class TripPolicy < ApplicationPolicy
       scope.joins(:trip_participants).where({ trip_participants: { user: user } })
     end
   end
+
+  def create?
+    true
+  end
+
+  def show?
+    true
+  end
 end
