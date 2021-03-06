8c52d7892cb04d5000e120f2ec5bd193383f1e3c
rachel herlihy <rachellouise.herlihy@gmail.com>
2021-03-06 14:21:00 +0000

    potential destinations controller, policy & routes

---
+++ app/policies/potential_destination_policy.rb
@@ -0,0 +1,15 @@
+class PotentialDestinationPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    true
+  end
+
+  def show?
+    true
+  end
+end
