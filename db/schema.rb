3d1fc856dfc0ddae317a8a3db84a4483756944d9
akilchomoko <akil@chomoko.com>
2021-03-12 07:44:06 +0000

    Setup for REDIS on Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_185831) do
+ActiveRecord::Schema.define(version: 2021_03_12_072548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -137,6 +137,7 @@ ActiveRecord::Schema.define(version: 2021_03_04_185831) do
     t.string "address_1"
     t.string "address_2"
     t.string "postcode"
+    t.boolean "admin", default: false, null: false
     t.index ["city_id"], name: "index_users_on_city_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
