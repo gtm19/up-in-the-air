6fbea6bae0fbb2a35a87dfbf1114a820f2269d82
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-03-13 09:00:58 +0000

    reorder has_many and add has_many :date_preferences

--- app/models/trip_participant.rb
+++ app/models/trip_participant.rb
@@ -1,6 +1,7 @@
 class TripParticipant < ApplicationRecord
   belongs_to :user
   belongs_to :trip
-  has_many :potential_destinations, dependent: :destroy
   has_many :participant_scores, dependent: :destroy
+  has_many :potential_destinations, dependent: :destroy
+  has_many :date_preferences, dependent: :destroy
 end
