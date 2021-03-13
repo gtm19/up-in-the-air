11eaf2a13076fc1a4dce7319ec459f9cb26f02ea
akilchomoko <akil@chomoko.com>
2021-03-13 18:54:56 +0000

    toggled love and unloved plus submission buttons

--- app/controllers/potential_destinations_controller.rb
+++ app/controllers/potential_destinations_controller.rb
@@ -9,46 +9,49 @@ class PotentialDestinationsController < ApplicationController
       @trip_estimates = TripEstimate.limit(20)
     end
 
+    @cards = cards_with_love
+
     @potential_destination = policy_scope(PotentialDestination)
   end
 
-  # def new
-  #   @city = City.find(params[:id])
-  #   @potential_destination = PotentialDestination.new
-  #   authorize @potential_destination
-  # end
 
   def create
     skip_authorization
-    tp = TripParticipant.find(params[:trip_participant_id])
-    te = TripEstimate.find(params[:est])
     pd = PotentialDestination.new
-    pd.city = te.destination_city
-    pd.trip_participant = tp
+    pd.city = TripEstimate.find(params[:est]).destination_city
+    pd.trip_participant = TripParticipant.find(params[:trip_participant_id])
     pd.status = 'selected'
     pd.save
     redirect_to trip_trip_participant_potential_destinations_path(params[:trip_id], params[:trip_participant_id])
   end
 
-    def destroy
-      skip_authorization
-      tp = TripParticipant.find(params[:trip_participant_id])
-      te = TripEstimate.find(params[:est])
-      pd = PotentialDestination.find_by(city: te.destination_city, trip_participant: tp)
-      pd.destroy
+  def destroy
+    skip_authorization
+    PotentialDestination.find(params[:id]).destroy
+    redirect_to trip_trip_participant_potential_destinations_path(params[:trip_id], params[:trip_participant_id])
+  end
 
-      redirect_to trip_trip_participant_potential_destinations_path(params[:trip_id], params[:trip_participant_id])
-
+  def update
+    skip_authorization
+    trip_participant = TripParticipant.find(params[:trip_participant_id])
+    pds = PotentialDestination.where(trip_participant: trip_participant, status: 'selected')
+    pds.each do |pd|
+      pd.status = 'submitted'
+      pd.save
     end
+    redirect_to trip_trip_participant_potential_destinations_path(params[:trip_id], params[:trip_participant_id])
+  end
 
-    def update
-      skip_authorization
-      redirect_to trip_path(params[:trip_id])
+  private
+
+  def cards_with_love
+    cards = []
+    @trip_estimates.each do |te|
+      card = {}
+      card[:te] = te
+      card[:pd] = PotentialDestination.find_by(city: te.destination_city, trip_participant: @trip_participant)
+      cards << card
     end
-
-    private
-
-    def params_cocktail
-      params.require(:cocktail).permit(:name, :instructions, :photo_url, :photo)
-    end
+    cards
+  end
 end
