25471551258743ab768135ca392a818866870a6b
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-03-13 15:57:47 +0000

    attempted select2 bugfix

--- app/javascript/components/select2.js
+++ app/javascript/components/select2.js
@@ -4,7 +4,8 @@ import 'select2/dist/css/select2.css';
 
 const initSelect2 = () => {
   $('.select2').select2({
-    placeholder: "Click and type to search"
+    placeholder: "Click and type to search",
+    width: "100%"
   }); // (~ document.querySelectorAll)
 };
 
