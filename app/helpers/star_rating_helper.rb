881a3977abe32440eef22cb60959f80046d8f9a5
Wayne Dalton <waynepatrickdalton@gmail.com>
2021-03-21 00:56:22 +0000

    Arranged padding on veto etc

--- app/helpers/star_rating_helper.rb
+++ app/helpers/star_rating_helper.rb
@@ -5,7 +5,7 @@ module StarRatingHelper
     num = num.round(1)
     whole_stars = num.floor
     half_star = ((num - whole_stars) * 2).to_i
-    empty_stars = (( num - (whole_stars + half_star))).to_i
+    empty_stars = ( 5 - (whole_stars + half_star)).to_i
     # TODO Fix the stars padding
     icon("fa", "star") * whole_stars + icon("fa", "star-half-alt") * half_star + icon("far", "star") * empty_stars
   end
