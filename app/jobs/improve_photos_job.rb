3875b792ce296705daefd6eaa27245d2ef56f918
akilchomoko <akil@chomoko.com>
2021-03-27 13:12:42 +0000

    Job to change photos of select cities

--- app/jobs/improve_photos_job.rb
+++ app/jobs/improve_photos_job.rb
@@ -10,10 +10,10 @@ class ImprovePhotosJob < ApplicationJob
     "https://res.cloudinary.com/dr4pzn94d/image/upload/v1615012711/p9_square_c8bptu.jpg",
     "https://res.cloudinary.com/dr4pzn94d/image/upload/v1615012710/p4_gate_berlin_v6bcvn.jpg",
     "https://res.cloudinary.com/dr4pzn94d/image/upload/v1615012710/p6_countryside_lwpc9k.jpg",
-    "https://res.cloudinary.com/dr4pzn94d/image/upload/v1615012710/p7_coast_lnw0qg.jpg",
-    "https://res.cloudinary.com/dr4pzn94d/image/upload/v1615012710/p8_city_bm286f.jpg",
-    "https://res.cloudinary.com/dr4pzn94d/image/upload/v1615012710/p5_dessert_trngk9.jpg",
-    "https://res.cloudinary.com/dr4pzn94d/image/upload/v1615012710/p3_houses_dutch_edtg7m.jpg"
+    "https://res.cloudinary.com/dr4pzn94d/image/upload/v1616847040/alpe-unsplash_zwwfd0.jpg",
+    "https://res.cloudinary.com/dr4pzn94d/image/upload/v1616847191/paris-unsplash_1_tqb7p3.jpg",
+    "https://res.cloudinary.com/dr4pzn94d/image/upload/v1616847046/lisbon-unsplash_iikklf.jpg",
+    "https://res.cloudinary.com/dr4pzn94d/image/upload/v1616847048/madrid-unsplash_zg0bzu.jpg"
   ]
 
   def change_photo(c, index)
@@ -30,6 +30,9 @@ class ImprovePhotosJob < ApplicationJob
   def perform
     change_photo(City.find_by(airport_code: "LHR"), 0)
     change_photo(City.find_by(airport_code: "SXF"), 5)
+    change_photo(City.find_by(airport_code: "LIS"), 9)
+    change_photo(City.find_by(airport_code: "AHZ"), 7)
+    change_photo(City.find_by(airport_code: "MAD"), 10)
+    change_photo(City.find_by(airport_code: "CDG"), 8)
   end
-
 end
